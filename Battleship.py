3b81bf76328c144cc73dcd202cfda05dc689c778
Moses Yiblet <yibletm@gmail.com>
2026-07-23 19:46:49 -0400

    BattleShip Prompt Made

---
+++ Battleship.py
@@ -0,0 +1,17 @@
+print("Battleship!!!")
+
+players = input("Select between 1 or 2 players: ")
+if players != "2"  and players != "2" :
+    while players != "1" and players != "2":
+        players = input("Select between 1 or 2 players: ")
+
+print("1) Easy Mode")
+print("2) Medium Mode")
+print("3) Hard Mode")
+
+mode = input("Select a diffculty mode from the following above: ")
+if mode != "1" and mode != "2" and mode != "3":
+    while mode != "1" and mode != "2" and mode != "3":
+        mode = mode = input("Select a diffculty mode from the following above: ")
+
+        
