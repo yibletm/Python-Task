ffb22823db3d5e4a59d5811608a9ea0e2a660356
Moses Yiblet <yibletm@gmail.com>
2026-07-25 10:05:14 -0400

    Player Boards Made

--- Battleship.py
+++ Battleship.py
@@ -14,4 +14,20 @@ if mode != "1" and mode != "2" and mode != "3":
     while mode != "1" and mode != "2" and mode != "3":
         mode = mode = input("Select a diffculty mode from the following above: ")
 
-        
+board = []
+length = 0
+if players == "1":
+    if mode == "1":
+        board = [["O"] * 5 for _ in range(5)]
+        length = 5
+    if mode == "2":
+            board = [["O"] * 8 for _ in range(8)]
+            length = 8 
+    if mode == "3":
+            board = [["O"] * 11 for _ in range(11)]
+            length = 11
+
+    for row in board:
+        print(" ".join(row))
+    
+
