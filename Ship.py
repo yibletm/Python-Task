07597eb8a1e6f2e6dd8fd0b58032a095b7958e5a
Moses Yiblet <yibletm@gmail.com>
2026-07-29 15:34:55 -0400

    Ship class completion

--- Ship.py
+++ Ship.py
@@ -3,4 +3,9 @@ class Ship:
     def __init__(self, length, orient):
      self.length = length
      self.orient = orient
-    
+
+    def getLength(self):
+        return self.length
+
+    def getOrient(self):
+        return self.orient
