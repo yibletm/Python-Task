4ee927c37303682b61e620164d18891e4264fbc5
Moses Yiblet <yibletm@gmail.com>
2026-08-24 20:43:29 -0400

    Game basically Completed

--- Battleship.py
+++ Battleship.py
@@ -36,14 +36,78 @@ if players == "1":
     for row in board:
         print(" ".join(row))
 
+    Targets = 0
 
-    Ship1 = Ship(3, length, boardships)
+    Ship1 = Ship(2, length, boardships)
     boardships = Ship1.setBoard(boardships)
+    Targets += 2
     Ship2 = Ship(3, length, boardships)
     boardships = Ship2.setBoard(boardships)
-    for row in boardships:
-        print(" ".join(row))
+    Targets += 3
+    if mode == "2":
+        Ship3 = Ship(3,length,boardships)
+        boardships = Ship3.setBoard(boardships)
+        Targets += 3
+        Ship4 = Ship(3,length,boardships)
+        boardships = Ship4.setBoard(boardships)
+        Targets += 3
+    if mode == "3":
+        Ship5 = Ship(4,length,boardships)
+        boardships = Ship5.setBoard(boardships)
+        Targets += 4
+        Ship6 = Ship(5,length,boardships)
+        boardships = Ship6.setBoard(boardships)
+        Targets += 5
+        Ship7 = Ship(5,length,boardships)
+        boardships = Ship7.setBoard(boardships)
+        Targets += 5
+
     
+    if mode == "1":
+        Shots = 15
+    if mode == "2":
+        Shots = 38
+    if mode == "3":
+        Shots = 50
+
+    Hits = 0
+    for row in board:
+        print(" ".join(row))
+    while Shots != 0 and Hits != Targets:
+
+        print(f"{Shots} shots left")
+        x = int(input("Enter the X coordinate on the board:"))
+        y = int(input("Now, Enter the Y coordinate on the board:"))
+        if x >= length or y >= length or board[x][y] != "~":
+            Newcoord = False
+            while Newcoord == False:
+                x = int(input("Enter the X coordinate on the board:"))
+                y = int(input("Now, Enter the Y coordinate on the board:"))
+                if (x < length or y < length) and board[x][y] == "~":
+                    Newcoord = True
+
+        if boardships[x][y] == "S":
+            board[x][y] = "X"
+            Hits += 1
+            Shots -= 1
+            for row in board:
+                print(" ".join(row))
+            print("A ship has been hit!!!")
+        else:
+            board[x][y] = "O"
+            Shots -= 1
+            for row in board:
+                print(" ".join(row))
+            print("Missed...")
+
+
+    if Hits == Targets:
+        print("YOU WIN!!!")
+    else:
+        print("you lose...")
+                    
+
+
         
     
     
